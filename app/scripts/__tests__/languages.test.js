608d981b9141f5b4ea8a2972d8147e72cdaf1d09
James Starkie <james.starkie@onthebeach.co.uk>
2017-06-19 15:42:45 +0100

    Add languages list

---
+++ app/scripts/__tests__/languages.test.js
@@ -0,0 +1,13 @@
+import languages from '../languages';
+
+it('has "popular" and "all" languages', () =>
+  expect(Object.keys(languages)).toEqual(['popular', 'all'])
+);
+
+it('has the correct format for all language options', () => {
+  const all = languages.popular.concat(languages.all);
+
+  all.forEach(language =>
+    expect(Object.keys(language)).toEqual(['name', 'code'])
+  );
+});
---
+++ app/scripts/languages.js
@@ -0,0 +1,94 @@
+module.exports = {
+  popular: [
+    {
+      name: 'English',
+      code: 'en',
+    },
+    {
+      name: 'Fran\u00e7ais',
+      code: 'fr',
+    },
+    {
+      name: 'Deutsch',
+      code: 'de',
+    },
+    {
+      name: 'Espa\u00f1ol',
+      code: 'es',
+    },
+    {
+      name: 'Italiano',
+      code: 'it',
+    },
+    {
+      name: 'Polski',
+      code: 'pl',
+    },
+  ],
+  all: [
+    {
+      name: '\u0411\u044a\u043b\u0433\u0430\u0440\u0441\u043a\u0438 \u0435\u0437\u0438\u043a',
+      code: 'bg',
+    },
+    {
+      name: 'Ce\u0161tina',
+      code: 'cs',
+    },
+    {
+      name: 'Dansk',
+      code: 'da',
+    },
+    {
+      name: 'suomi, suomen kieli',
+      code: 'fi',
+    },
+    {
+      name: 'Magyar',
+      code: 'hu',
+    },
+    {
+      name: 'Bahasa Indonesia',
+      code: 'id',
+    },
+    {
+      name: '\u65e5\u672c\u8a9e',
+      code: 'ja',
+    },
+    {
+      name: 'Nederlands, Vlaams',
+      code: 'nl',
+    },
+    {
+      name: 'Norsk',
+      code: 'no',
+    },
+    {
+      name: 'Portugu\u00eas',
+      code: 'pt',
+    },
+    {
+      name: 'Rom\u00e2n\u0103',
+      code: 'ro',
+    },
+    {
+      name: '\u0420\u0443\u0441\u0441\u043a\u0438\u0439',
+      code: 'ru',
+    },
+    {
+      name: 'Slovensk\u00e1',
+      code: 'sk',
+    },
+    {
+      name: 'Svenska',
+      code: 'sv',
+    },
+    {
+      name: '\u7b80\u4f53\u4e2d\u6587',
+      code: 'zh',
+    },
+    {
+      name: '\u7e41\u9ad4\u4e2d\u6587',
+      code: 'zh',
+    },
+  ],
+};
